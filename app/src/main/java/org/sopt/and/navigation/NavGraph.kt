846827f550160429d915564cdf0909ba809f9046
beom84 <cjja00@konkuk.ac.kr>
2024-10-21 00:16:52 +0900

    [feat] #4  bottomBar 구현

--- app/src/main/java/org/sopt/and/navigation/NavGraph.kt
+++ app/src/main/java/org/sopt/and/navigation/NavGraph.kt
@@ -1,15 +1,20 @@
-package org.sopt.and
+package org.sopt.and.navigation
 
+import android.util.Log
 import androidx.compose.runtime.Composable
 import androidx.navigation.NavHostController
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.toRoute
 import com.google.accompanist.permissions.ExperimentalPermissionsApi
+import org.sopt.and.screen.Home
+import org.sopt.and.screen.HomeScreen
 import org.sopt.and.screen.LogIn
 import org.sopt.and.screen.LogInScreen
-import org.sopt.and.screen.MyPage
+import org.sopt.and.screen.MyProFile
 import org.sopt.and.screen.MyProfileScreen
+import org.sopt.and.screen.Search
+import org.sopt.and.screen.SearchScreen
 import org.sopt.and.screen.SignUp
 import org.sopt.and.screen.SignUpScreen
 import org.sopt.and.viewmodel.SignUpState
@@ -17,18 +22,19 @@ import org.sopt.and.viewmodel.SignUpState
 
 @OptIn(ExperimentalPermissionsApi::class)
 @Composable
-fun NavGraph(navController: NavHostController) {
+fun NavGraph(navController: NavHostController, bottomBarVisible: (Boolean) -> Unit) {
     NavHost(navController = navController, startDestination = LogIn("", "")) {
         composable<LogIn> { backStackEntry ->
             val item = backStackEntry.toRoute<LogIn>()
 
             LogInScreen(
                 navigateToMyPage = { email ->
-                    navController.navigate(MyPage(email)) {
-                            popUpTo<LogIn> {
-                                inclusive = true
+                    navController.navigate(MyProFile(email)) {
+                        popUpTo<LogIn> {
+                            inclusive = true
                         }
                     }
+                    bottomBarVisible(true)
                 },
                 navigateToSignUp = {
                     navController.navigate(SignUp)
@@ -41,7 +47,7 @@ fun NavGraph(navController: NavHostController) {
             SignUpScreen(
                 navigateToLogIn = { id, password ->
                     navController.navigate(LogIn(id, password)) {
-                        popUpTo(SignUp){inclusive = true}
+                        popUpTo(SignUp) { inclusive = true }
                         launchSingleTop = true
                     }
                 },
@@ -51,9 +57,15 @@ fun NavGraph(navController: NavHostController) {
             )
         }
 
-        composable<MyPage> { backStackEntry ->
-            val item = backStackEntry.toRoute<MyPage>()
+        composable<MyProFile> { backStackEntry ->
+            val item = backStackEntry.toRoute<MyProFile>()
             MyProfileScreen(item.email)
+        }
+        composable<Home> {
+            HomeScreen()
+        }
+        composable<Search> {
+            SearchScreen()
         }
     }
 }
