de86308a74831113e78a86eb42c91fad5c07de09
beom84 <cjja00@konkuk.ac.kr>
2024-11-08 02:48:39 +0900

    [feat] #8 ApiModule 추가, DataSourceModule 추가

---
+++ app/src/main/java/org/sopt/and/di/DataSourceModule.kt
@@ -0,0 +1,18 @@
+package org.sopt.and.di
+
+import dagger.Binds
+import dagger.Module
+import dagger.hilt.InstallIn
+import dagger.hilt.components.SingletonComponent
+import org.sopt.and.data.dataremote.datasouceimpl.UserServiceRemoteDataSourceImpl
+import org.sopt.and.data.dataremote.datasource.UserServiceRemoteDataSource
+import org.sopt.and.data.repositoryimpl.UserServiceRepositoryImpl
+import javax.inject.Singleton
+
+@Module
+@InstallIn(SingletonComponent::class)
+abstract class DataSourceModule {
+    @Binds
+    @Singleton
+    abstract fun bindUserServiceRemoteDataSource(userServiceRemoteDataSourceImpl: UserServiceRemoteDataSourceImpl): UserServiceRemoteDataSource
+}
